6db02c374d2e67a25f1fa3d384e6e98b94edc757
eyaBs93 <eyabensaid20@gmail.com>
2018-10-21 18:18:44 +0100

    changes in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,17 @@
-const readline = require([],'readline');
-const rl = readline.createInterface(
-  process.stdin,
-  process.stdout
-);
+/*const readline = require([],'readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 rl.question('Salut, bienvenue dans ma super pyramide ! Combien d\'étages veux-tu ? \n', (answer) => {
   // TODO: Log the answer in a database
   console.log(`Thank you for your valuable feedback: ${answer}`);
   rl.close();
 });
-
-/*ourheight = prompt('Salut, bienvenue dans ma super pyramide ! Combien d\'étages veux-tu ? \n');
-pyramid(ourheight);
 */
+ourheight = prompt('Salut, bienvenue dans ma super pyramide ! Combien d\'étages veux-tu ? \n');
+pyramid(ourheight);
 //function build a pyramid with a height of 
 function pyramid(height) {
     let ourPyramid='';
@@ -24,7 +23,7 @@ function pyramid(height) {
             for (let i = 1; i <= height ; i++) {
                 ourPyramid += writeCharN(' ' , height-i) + writeCharN('#' , 2*i-1) + "\n";
             }
-            console.log("voici la pyramide: \n" + ourPyramid);
+            console.log('Salut, bienvenue dans ma super pyramide ! Combien d\'étages veux-tu ? \n' + height +'\n voici la pyramide: \n' + ourPyramid);
         } else {
             console.log('oooh! la hauteur est très grand, je ne peux contruire une pyramide avec un hauteur supérieur à 25');
         }   
@@ -35,9 +34,9 @@ function pyramid(height) {
  //fonction qui retourne une chaine qui présente la variable ourChar écrite n fois
  function writeCharN(ourChar , n) {
     let ourString = '';
-     for (let i = 1; i <= n ; i++) {
-         ourString += ourChar; 
-     }
+    for (let i = 1; i <= n ; i++) {
+        ourString += ourChar; 
+    }
     return ourString;
  }
 
