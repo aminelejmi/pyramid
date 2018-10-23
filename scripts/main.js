5730e686bbecfa660cf9e952d76090190618841b
Ben said EYA <41062890+eya93@users.noreply.github.com>
2018-10-23 10:08:14 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,3 @@
-/*const readline = require([],'readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Salut, bienvenue dans ma super pyramide ! Combien d\'étages veux-tu ? \n', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  rl.close();
-});
-*/
-
 //fonction qui construit la pyramide avec l'hauteur height donnée
 function pyramid(height) {
     let ourPyramid='';
@@ -20,7 +7,7 @@ function pyramid(height) {
         height = parseInt(height);
         if (height < 25) {
             for (let i = 1; i <= height ; i++) {
-                ourPyramid += writeCharN(' ' , height-i) + writeCharN('#' , 2*i-1) + "\n";
+                ourPyramid += ' '.repeat(height-i) + '#'.repeat(2*i-1) + "\n";
             }
             console.log('Salut, bienvenue dans ma super pyramide ! Combien d\'étages veux-tu ? \n' + height +'\n voici la pyramide: \n' + ourPyramid);
         } else {
@@ -30,13 +17,6 @@ function pyramid(height) {
         console.log('vous devez saisir un entier');
     }
  }
- //fonction qui retourne une chaine qui présente la variable ourChar écrite n fois
- function writeCharN(ourChar , n) {
-    let ourString = '';
-    for (let i = 1; i <= n ; i++) {
-        ourString += ourChar; 
-    }
-    return ourString;
- }
+ 
 ourheight = prompt('Salut, bienvenue dans ma super pyramide ! Combien d\'étages veux-tu ? \n');
 pyramid(ourheight);
